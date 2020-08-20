600fd029e5f444d2e8613662fb51a37a8663f9fb
fahimiah34 <59512314+fahimiah34@users.noreply.github.com>
2020-08-20 19:19:35 -0400

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,1 @@
+prompt("This tab is where you can learn about how to contact us at Generation Why. If you are on the right tab, write yes. If not, write no");
---
+++ team.js
@@ -0,0 +1,1 @@
+prompt("This tab goes in depth on each department in Generation Why. If you are on the right tab, write yes. If not, write no");
